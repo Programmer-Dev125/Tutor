b6c79c3fc5abd28c56617709aaef1c8c8cd97aa1
Programmer-Dev125 <programmerdev125@gmail.com>
2025-03-28 14:24:44 -0500

    sm

--- src/components/courses/courses.jsx
+++ src/components/courses/courses.jsx
@@ -5,9 +5,9 @@ export default function Courses() {
       <section className="g3 g20">
         <div className="card">
           <img src="/physics-picture.jpg" alt="Physics" />
-          <h2 className="course-title text-center mb10">English</h2>
+          <h2 className="course-title text-center mb10">Physics</h2>
           <p className="title text-center mt0 mb0 pr10 pl10">
-            Grammar, writing, comprehension, and communication skills
+            Mechanics, electricity, magnetism, and problem-solving techniques
           </p>
           <div className="icon"></div>
           <div className="icon-left"></div>
@@ -16,7 +16,7 @@ export default function Courses() {
           <img src="/maths-picture.jpg" alt="Maths Picture" />
           <h2 className="course-title text-center mb10">Mathematics</h2>
           <p className="title text-center mt0 mb0 pr10 pl10">
-            Mechanics, electricity, magnetism, and problem-solving techniques
+            Algebra, calculus, geometry, and real-world applications
           </p>
           <div className="icon"></div>
           <div className="icon-left"></div>
@@ -25,7 +25,7 @@ export default function Courses() {
           <img src="/english-picture.jpg" alt="Maths Picture" />
           <h2 className="course-title text-center mb10">English</h2>
           <p className="title text-center mt0 mb0 pr10 pl10">
-            Algebra, calculus, geometry, and real-world applications
+            Grammar, writing, comprehension, and communication skills
           </p>
           <div className="icon"></div>
           <div className="icon-left"></div>
